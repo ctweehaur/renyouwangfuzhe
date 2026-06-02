c590a7c8e5bc5871341075a181e2625c180f783a
ctweehaur <ctweehaur@gmail.com>
2026-06-02 19:49:05 +0800

    Create renyouwangfuzhe_translation.js

---
+++ renyouwangfuzhe_translation.js
@@ -0,0 +1,12 @@
+const translationData = [
+    {
+        zh: "从前有一个丢了斧头的人，主观地怀疑是邻居家的儿子偷去的。于是他暗中观察那个孩子：看他走路的姿势，怎么看都像是偷了斧头的；看他的面部表情，也像是偷了斧头的；听他说话的语气，更像是偷了斧头的；全身上下一举一动、一神一态，没有一处不像是一个偷斧头的人。",
+        py: "xī qián yǒu yī gè diū le fǔ tóu de rén zhǔ guān de huái yí shì lín jū jiā de ér zǐ tōu qù de yú shì tā àn zhōng guān chá nà gè hái zǐ kàn tā zǒu lù de zī shì zěn me kàn dōu xiàng shì tōu le fǔ tóu de kàn tā de miàn bù biǎo qíng yě xiàng shì tōu le fǔ tóu de tīng tā shuō huà de yǔ qì gèng xiàng shì tōu le fǔ tóu de quán shēn xià xià yī jǔ yī dòng yī shén yī tài méi yǒu yī chù bú xiàng shì yī gè tōu fǔ tóu de rén",
+        en: "Once, a man lost his axe and suspected his neighbor's son of stealing it. He then closely observed the boy: his manner of walking was that of an axe-thief; his facial expression was that of an axe-thief; his speech sounded like that of an axe-thief; in short, every single gesture and demeanor he made suggested nothing but a person who had stolen the axe."
+    },
+    {
+        zh: "过了不久，这个丢斧头的人在挖掘自家的水沟时，竟然意外找到了自己丢失的斧头。又过了几天，他再次见到邻居家的儿子，发现这孩子的言行举止和神态表情，怎么看都不像是一个偷斧头的人了。",
+        py: "guò le bù jiǔ zhè gè diū fǔ tóu de rén zài wā jué zì jiā de shuǐ gōu shí jìng rán yì wài zhǎo dào le zì jǐ diū shī de fǔ tóu yòu guò le jǐ tiān tā zài cì jiàn dào lín jū jiā de ér zǐ fā xiàn zhè hái zǐ de yán xíng jǔ zhǐ hé shén tài biǎo qíng zěn me kàn dōu bú xiàng shì yī gè tōu fǔ tóu de rén le",
+        en: "Shortly afterwards, while clearing out his own ditch, the man unexpectedly discovered his missing axe. When he saw his neighbor's son again a few days later, he noticed that the boy's behavior and demeanor looked nothing like those of an axe-thief at all."
+    }
+];
